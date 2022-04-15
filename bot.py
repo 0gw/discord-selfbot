bef8608890f6597b27068be5cf3f85508f92e4a1
colin <90219288+0gw@users.noreply.github.com>
2022-04-15 05:26:29 +0200

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,17 @@
+﻿import os
+
+import discord
+from discord.ext import commands
+import yaml
+
+
+settings = yaml.safe_load(open("settings.yml", "r").read())
+
+bot = commands.Bot(command_prefix='.', 
+                   self_bot=True,
+                   case_sensitive=False)
+
+for filename in os.listdir('./commands'):
+    if filename.endswith('.py'):
+        bot.load_extension(f'commands.{filename[:-3]}')
+bot.run(settings['token'], bot=False)
